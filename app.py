0b76aa07b9b0b0f0b622bbefe30a322862a66d34
lindseyma <lma2@stuy.edu>
2016-12-03 17:16:34 -0500

    css finally works

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app= Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/result/')
+def result():
+	return render_template('result.html')
+
 if __name__ == "__main__": 
 	app.debug = True
 	app.run()
