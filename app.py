88125664133e17080480b3312e1e3e76832bb7f1
Janet Zhang <jzhang12@stuy.edu>
2016-12-03 12:16:44 -0500

    app test

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, url_for, session, redirect
+
+app= Flask(__name__)
+
+@app.route("/")
+@app.route("/home/", methods = ['POST'])
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__": 
+	app.debug = True
+	app.run()
